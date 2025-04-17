85429ec24f1de26c4cf960769fa10817bfe9e1ee
jollybuoy <jollybuoytech@gmail.com>
2025-04-16 21:45:22 -0400

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -53,7 +53,7 @@ router.post('/login', async (req, res) => {
     const token = jwt.sign(
       {
         id: user.id,
-        email: user.email,
+        email: user.id,
         name: user.name, // ✅ Name added correctly
         role: user.role,
         domain: user.domain,

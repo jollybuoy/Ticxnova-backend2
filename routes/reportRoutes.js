75feff4b12f9cfef6c2173007902b938837d185f
jollybuoy <jollybuoytech@gmail.com>
2025-04-29 11:10:46 -0400

    Update reportRoutes.js

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -1,1 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const { getReportSummary } = require('../controllers/reportController');
+const authenticateToken = require('../middleware/auth');
+
+router.get('/summary', authenticateToken, getReportSummary);
+
+module.exports = router;
 
